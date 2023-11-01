63b8d20779b5bb48c41f7d0e7be5afdc4af71588
Migue Moyano <miguel.moyano@etermax.com>
2023-11-01 18:48:54 -0300

    build

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "https://elmiguedev.github.io/streamsoide-app/",
   plugins: [react()],
 })
